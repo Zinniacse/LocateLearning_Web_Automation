525c4acfcc4d2e9575f9fbd1b4273b110e6a8ce1
unknown <zinnia.cse7862@gmail.com>
2022-06-25 13:19:33 +0600

    checkbox and radio button verify for web

---
+++ src/main/java/test_cases/Checkbox_RadioButton.java
@@ -0,0 +1,41 @@
+package test_cases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import base.Driver_Setup;
+
+public class Checkbox_RadioButton extends Driver_Setup {
+	public static String baseUrl = "https://jqueryui.com/checkboxradio";
+
+	@Test
+	public static void checkBoxAndRadioButtonInIFrame() throws InterruptedException {
+		driver.get(baseUrl);
+		new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+
+		Thread.sleep(5000);
+		// Switch to Frame
+		driver.switchTo().frame(0);
+		// Operate Elements
+
+		driver.findElement(By.xpath("//body[1]/div[1]/fieldset[1]/label[3]/span[1]")).click();
+		//driver.findElement(By.xpath("/html[1]/body[1]/div[1]/fieldset[1]/label[3]/span[1]")).click();
+
+		// label[@for='radio-3']
+
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//body[1]/div[1]/fieldset[2]/label[4]/span[1]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//body[1]/div[1]/fieldset[2]/label[1]/span[1]")).click();
+		// body[1]/div[1]/fieldset[2]/label[4]/span[1]
+		Thread.sleep(5000);
+		// Back to page
+		driver.switchTo().defaultContent();
+		// Close site
+		driver.close();
+	}
+}
